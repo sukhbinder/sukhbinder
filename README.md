56d9ff64e6ac153133df42da8e2a64d247926cab
Sukhbinder Singh <sukhbindersingh@gmail.com>
2025-04-14 17:22:45 +0530

    New readme builder borrowed from simonw

--- README.md
+++ README.md
@@ -7,32 +7,35 @@
 Autodidactic, lifelong learner, tinkerer, experimental cook and aspirational enterpreneur. This is my webspace, where I share some of my personal projects. 
 
 - 🔭 I’m currently working on developing software that enables solving of engineering problems.
-- 🌱 I’m currently learning kanada/german and web development
-- 👯 I’m looking to collaborate on my personal projects
+- 🌱 I’m currently learning kanada and web development
 - 💬 Ask me about python, helping kids study etc.
-- 📫 How to reach me: [aerogeek](http://www.twitter.com/aerogeek)
-- ⚡ [Read a random post from my blog](https://sukhbinder.wordpress.com/?random)
 
-## My Latest Blog posts and Youtube Videos.
-<!-- BLOG-POST-LIST:START -->
-- [Vaisakhi celebration at Gurudwara Singh Sabha BTM Community Hall Bangalore #humhairahi #travel](https://www.youtube.com/watch?v=9Y6IcXalMyE)
-- [Linus Torvalds built Git in 10 days – and never imagined it would last 20 years](https://sukhbinder.wordpress.com/2025/04/11/linus-torvalds-built-git-in-10-days-and-never-imagined-it-would-last-20-years/)
-- [winzy visual notify v0.0.4 released](https://sukhbinder.wordpress.com/2025/04/11/winzy-visual-notify-v0-0-4-released/)
-- [Thirumalai Naicker Palace | Madurai | Meenakshi Temple | Tamil Nadu travel | kia sonet #humhairahi](https://www.youtube.com/watch?v=SKvdm6rzFmI)
-- [Invert, Always Invert!](https://sukhbinder.wordpress.com/2025/04/08/invert-always-invert/)
-- [My Impressions of Llama 4](https://sukhbinder.wordpress.com/2025/04/07/my-impressions-of-llama-4/)
-- [Thirumalai Naicker Palace | Meenakshi temple | Madurai day 0 #TamilNadu #travel  #humhairahi](https://www.youtube.com/watch?v=l4oGPm1qqkI)
-- [Start using local LLM using my smollama-py v0.0.1](https://sukhbinder.wordpress.com/2025/04/05/start-using-local-llm-using-my-smollama-py-v0-0-1/)
-- [What’s the Weather](https://sukhbinder.wordpress.com/2025/04/03/whats-the-weather/)
-- [Shri kouchkal Muneshwara Swami Gudi | Offbeat Location | Cave temple](https://www.youtube.com/watch?v=oSfbHMXDcz8)
-- [Roadtrip to Madurai #TamilNaduTravel #humhairahi #kiasonet](https://www.youtube.com/watch?v=5j7dxAfbhUs)
-- [Save the Web by Being Nice.](https://sukhbinder.wordpress.com/2025/03/31/save-the-web-by-being-nice/)
-- [Dandiganahalli Dam | Just 90km |  #travel #picnicspot #halfday #daytrip #camping](https://www.youtube.com/watch?v=ydnNWBLBjvA)
-- [Dandiganahalli Dam | Just 90km |  #travel #picnicspot #halfday #daytrip #camping](https://www.youtube.com/watch?v=pdr8N_r9XEA)
-- [The real needs of life ….](https://sukhbinder.wordpress.com/2025/03/28/the-real-needs-of-life/)
-- [I’m leading a double life……](https://sukhbinder.wordpress.com/2025/03/26/im-leading-a-double-life/)
-- [Shri Kouchkal Muneshwara Swami Gudi | Mudishwara Temple | Just 80km from bangalore #humhairahi](https://www.youtube.com/watch?v=gyC7gMUpYzs)
-- [TIL: Python Input issue : EOFError](https://sukhbinder.wordpress.com/2025/03/23/til-python-input-issue-eoferror/)
-- [Dandiganahalli Dam | Just 90km |  #travel #picnicspot #halfday #daytrip #camping](https://www.youtube.com/watch?v=n6aDg4t5NAY)
-- [Notes for Version 0.1.5 of llm-duckduckgo-chat](https://sukhbinder.wordpress.com/2025/03/22/notes-for-version-0-1-5-of-llm-duckduckgo-chat/)
-<!-- BLOG-POST-LIST:END -->
+
+Read [my blog](https://sukhbinder.wordpress.com/), follow me on [X](https://x.com/aerogeek) or on [Bluesky](https://bsky.app/profile/sukhbinder.bsky.social).
+
+
+
+<table><tr><td valign="top" width="33%">
+
+### Recent releases
+<!-- recent_releases starts -->
+
+<!-- recent_releases ends -->
+More [recent releases](https://github.com/sukhbinder/sukhbinder/blob/main/releases.md)
+</td><td valign="top" width="34%">
+
+### On my blog
+<!-- blog starts -->
+
+<!-- blog ends -->
+More on [sukhbinder.wordpress.com](https://sukhbinder.wordpress.com/)
+</td><td valign="top" width="33%">
+
+### Youtube
+<!-- youtube starts -->
+
+<!-- youtube ends -->
+More on [youtube.com/@humhairahi](https://www.youtube.com/@humhairahi)
+</td></tr></table>
+
+<a href="https://github.com/sukhbinder/sukhbinder/actions"><img src="https://github.com/sukhbinder/sukhbinder/workflows/Build%20README/badge.svg" align="right" alt="Build README"></a> <a href="https://sukhbinder.wordpress.com/?random">Read a random post from my blog</a>
---
+++ build_readme.py
@@ -0,0 +1,182 @@
+from python_graphql_client import GraphqlClient
+import feedparser
+import httpx
+import json
+import pathlib
+import re
+import os
+
+root = pathlib.Path(__file__).parent.resolve()
+client = GraphqlClient(endpoint="https://api.github.com/graphql")
+
+TOKEN = os.environ.get("SUKHBINDER_TOKEN", "")
+
+SKIP_REPOS = {
+  
+}
+
+
+def replace_chunk(content, marker, chunk, inline=False):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = "\n{}\n".format(chunk)
+    chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(marker, chunk, marker)
+    return r.sub(chunk, content)
+
+
+GRAPHQL_SEARCH_QUERY = """
+query {
+  search(first: 100, type:REPOSITORY, query:"is:public owner:sukhbinder sort:updated", after: AFTER) {
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
+    nodes {
+      __typename
+      ... on Repository {
+        name
+        description
+        url
+        releases(orderBy: {field: CREATED_AT, direction: DESC}, first: 1) {
+          totalCount
+          nodes {
+            name
+            publishedAt
+            url
+          }
+        }
+      }
+    }
+  }
+}
+"""
+
+
+def make_query(after_cursor=None, include_organization=False):
+    return GRAPHQL_SEARCH_QUERY.replace(
+        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
+    )
+
+
+def fetch_releases(oauth_token):
+    repos = []
+    releases = []
+    # Skip these repos:
+    repo_names = set(SKIP_REPOS)
+    has_next_page = True
+    after_cursor = None
+
+    first = True
+
+    while has_next_page:
+        data = client.execute(
+            query=make_query(after_cursor, include_organization=first),
+            headers={"Authorization": "Bearer {}".format(oauth_token)},
+        )
+        first = False
+        print()
+        print(json.dumps(data, indent=4))
+        print()
+        repo_nodes = data["data"]["search"]["nodes"]
+        for repo in repo_nodes:
+            if repo["releases"]["totalCount"] and repo["name"] not in repo_names:
+                repos.append(repo)
+                repo_names.add(repo["name"])
+                releases.append(
+                    {
+                        "repo": repo["name"],
+                        "repo_url": repo["url"],
+                        "description": repo["description"],
+                        "release": repo["releases"]["nodes"][0]["name"]
+                        .replace(repo["name"], "")
+                        .strip(),
+                        "published_at": repo["releases"]["nodes"][0]["publishedAt"],
+                        "published_day": repo["releases"]["nodes"][0][
+                            "publishedAt"
+                        ].split("T")[0],
+                        "url": repo["releases"]["nodes"][0]["url"],
+                        "total_releases": repo["releases"]["totalCount"],
+                    }
+                )
+        after_cursor = data["data"]["search"]["pageInfo"]["endCursor"]
+        has_next_page = after_cursor
+    return releases
+
+
+
+
+def fetch_feed_entries(feedurl):
+    entries = feedparser.parse(feedurl)["entries"]
+    return [
+        {
+            "title": entry["title"],
+            "url": entry["link"].split("#")[0],
+            "published": entry["published"].split("T")[0],
+        }
+        for entry in entries
+    ]
+
+
+if __name__ == "__main__":
+    readme = root / "README.md"
+    project_releases = root / "releases.md"
+    releases = fetch_releases(TOKEN)
+    releases.sort(key=lambda r: r["published_at"], reverse=True)
+    md = "\n\n".join(
+        [
+            "[{repo} {release}]({url}) - {published_day}".format(**release)
+            for release in releases[:8]
+        ]
+    )
+    readme_contents = readme.open().read()
+    rewritten = replace_chunk(readme_contents, "recent_releases", md)
+
+    # Write out full project-releases.md file
+    project_releases_md = "\n".join(
+        [
+            (
+                "* **[{repo}]({repo_url})**: [{release}]({url}) {total_releases_md}- {published_day}\n"
+                "<br />{description}"
+            ).format(
+                total_releases_md="- ([{} releases total]({}/releases)) ".format(
+                    release["total_releases"], release["repo_url"]
+                )
+                if release["total_releases"] > 1
+                else "",
+                **release
+            )
+            for release in releases
+        ]
+    )
+    project_releases_content = project_releases.open().read()
+    project_releases_content = replace_chunk(
+        project_releases_content, "recent_releases", project_releases_md
+    )
+    project_releases_content = replace_chunk(
+        project_releases_content, "project_count", str(len(releases)), inline=True
+    )
+    project_releases_content = replace_chunk(
+        project_releases_content,
+        "releases_count",
+        str(sum(r["total_releases"] for r in releases)),
+        inline=True,
+    )
+    project_releases.open("w").write(project_releases_content)
+
+    youtube = fetch_feed_entries("https://www.youtube.com/feeds/videos.xml?channel_id=UCMbEUhrtCc95WypjB8-JOFA")
+    youtube_md = "\n\n".join(
+        ["[{title}]({url}) - {published}".format(**entry) for entry in youtube]
+    )
+    
+    rewritten = replace_chunk(rewritten, "youtube", youtube_md)
+
+    entries = fetch_feed_entries("https://sukhbinder.wordpress.com/feed")[:6]
+    entries_md = "\n\n".join(
+        ["[{title}]({url}) - {published}".format(**entry) for entry in entries]
+    )
+    rewritten = replace_chunk(rewritten, "blog", entries_md)
+
+    readme.open("w").write(rewritten)
